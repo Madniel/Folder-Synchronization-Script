895c189c839725aa07557f56461c4d1797cebf6f
Michael <michalfeliks19@gmail.com>
2023-09-23 00:00:26 +0200

    First commit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='brightness_patch_detector',
+    name='sync_package',
     version='0.1',
     packages=find_packages(),
     install_requires=['pytest'],
