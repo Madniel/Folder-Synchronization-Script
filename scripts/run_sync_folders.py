c0d4acb7692fd069a3e3e3f96f78a5da1c302a55
madniel <michalfeliks19@gmail.com>
2023-09-24 14:39:43 +0200

    add docstrings to run script

--- scripts/run_sync_folders.py
+++ scripts/run_sync_folders.py
@@ -4,6 +4,20 @@ import time
 from sync_folders.sync_folders import sync_folders
 
 if __name__ == "__main__":
+    """
+    run_script: A script for folder synchronization.
+
+    This script continually synchronizes the contents of a source folder to a replica folder.
+    Users can specify the source and replica directories, synchronization interval, and the path to a log file.
+
+    Usage:
+        run_script.py <source_folder_path> <replica_folder_path> <synchronization_interval_in_seconds> <log_file_path>
+
+    Example:
+        run_script.py /path/to/source/folder /path/to/replica/folder 10 /path/to/logfile.log
+
+    This example will sync the source folder to the replica folder every 10 seconds and log the synchronization activities to the specified log file.
+    """
     parser = argparse.ArgumentParser(description="Run the folder synchronization.")
     parser.add_argument("source", help="Source folder path")
     parser.add_argument("replica", help="Replica folder path")
